dd79fb2f1241dda810ce24c143c1df2c8f609355
Brady DeBruyn <120288845+bradydebruyn@users.noreply.github.com>
2026-04-08 15:36:41 -0400

    Update bitcrusher.h

--- bitcrusher.h
+++ bitcrusher.h
@@ -3,9 +3,11 @@
 #define BITCRUSHER_H
 
 #include <stdint.h>
+#include "hls_stream.h"
+#include "ap_axi_sdata.h"
 
 // Audio sample: 16-bit signed (matches ADAU1761 codec output)
-typedef int16_t data_t;
+typedef int32_t data_t;
 
 // Number of bits to crush: small value (0–14 is meaningful for 16-bit audio)
 typedef uint8_t crush_t;
